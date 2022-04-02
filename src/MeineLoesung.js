f631552aee176265b84256e93afbf38f4349f97b
John <flnc85@hotmail.de>
2022-04-03 00:58:46 +0200

    was probiert

--- src/MeineLoesung.js
+++ src/MeineLoesung.js
@@ -1,4 +1,84 @@
 /*
+### 1
+Schreibe ein Programm, welches eine Person altersgerecht begrüsst.
+Die Begrüssung soll auf der Console ausgegeben werden.
+
+- Kinder, d.h. bis und mit 10 Jahren: Hoi <name>
+- Jugendliche, d.h. ab 11 Jahren bis und mit 18 Jahren: Hallo <name>
+- Erwachsene, d.h. ab 19 Jahren bis und mit 100 Jahren: Grüezi <name>
+- Alle anderen, d.h. ab 101 Jahren: Grüss Gott <name>
+
+Schreibe zwei Varianten,
+- eine Variante mit "else if",
+- eine Variante ohne Verwendung von "else if"
+
+Tip: &&
+ */
+// let alter = 10
+// let name = 'hans'
+// if(alter<=10){
+//     console.log("Hoi "+name)
+// }else if(alter>=11 && alter<=18){
+//     console.log("Hallo "+name)
+// }else if(alter>=19 && alter<=100){
+//     console.log("Grüezi "+name)
+// }else if(alter>=101){
+//     console.log("Grüss Gott "+name)
+// }
+//-----------------------------------------------------------------------------------
+// let alter = 10
+// let name = 'hans'
+/////////////////////////////////////////////////////////////////
+// switch (alter) {
+//     case (alter<=10):
+//         console.log("Hoi "+name)
+//         break;
+//     case alter>=11 && alter<=18:
+//         console.log("Hallo "+name)
+//         break;
+//     case (alter>=19 && alter<=100):
+//         console.log("Grüezi "+name)
+//         break;
+    // default:
+    //     console.log("Grüss Gott "+name)
+    //     break;
+//}
+//----------------------------------------------------------------------------------------------------
+/*### 3
+Schreibe ein Programm, welches entscheidet, wer in den Club darf und wer nicht
+- Männer dürfen ab 20 rein, Frauen bereits ab 18.
+Gib die Entscheidung auf der Console aus.
+
+Schreibe zwei Varianten,
+- eine Variante mit switch case
+- eine Variante mit && und || (ohne ganz ausgeschriebene if)
+*/
+// let ausgabe = ''
+// function tuerSteher(alter, geschlecht){
+//     if((alter>=20&&geschlecht=='m')||(alter>=18&&geschlecht=='f')){
+//         ausgabe='Dürfen rein'
+//     }else{
+//         ausgabe = 'Dürfen nicht rein'
+//     }
+// }
+//
+// tuerSteher(17,'f')
+// console.log(ausgabe)
+//-------------------------------------------------------------------------------------------
+/*
+### 0
+Schreibe ein Programm, welches jedes Zeichen eines Strings einzeln ausgibt, Komma getrennt.
+z.B. aus `"Pinguin"` wird `"P,i,n,g,u,i,n"`
+
+Schreibe zwei Varianten:
+- eine ohne Schlaufen zu verwenden
+- eine mit einer Schlaufe
+ */
+
+
+
+//------------------------------------------------------------------------------------
+/*
 ### 2
 Schreibe ein Programm, welches einen Array von Zahlen verarbeitet und in einen Array mit Booleans umwandelt.
 Das Programm ermittelt für jede Zahl ob diese gerade ist oder nicht.
@@ -115,17 +195,19 @@ Berechne den Durchschnitt (arithmetisches Mittel) aller Zahlen.
 //fibonacci
 //Eine unendliche Zahlenreihe, die mit 0 und 1 beginnt. Jede weitere Zahl entspricht dabei der Summe der beiden vorangegangenen Zahlen. Damit lautet der Anfang der Zahlenreihe 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 usw.
 
-let arr1 = [0,1]
+// let arr1 = [0,1]
+//
+// let arr2 = arr1
+// for(let i = 1; i<11;i++){
+//     arr2.push(arr1[i]+arr1[i-1])
+// }
+// console.log(arr2)
+// /*
+// [
+//    0,  1,  1,  2,  3,
+//    5,  8, 13, 21, 34,
+//   55, 89
+// ]
+//  */
 
-let arr2 = arr1
-for(let i = 1; i<11;i++){
-    arr2.push(arr1[i]+arr1[i-1])
-}
-console.log(arr2)
-/*
-[
-   0,  1,  1,  2,  3,
-   5,  8, 13, 21, 34,
-  55, 89
-]
- */
+//-------------------------------------------------------------------------------------
--- src/exercises/fibonacci.js
+++ src/exercises/fibonacci.js
@@ -2,3 +2,18 @@
 
 // Berechne die Fibonacci Folge für n Elemente, beginnend bei 0.
 // Gib alle berechneten Elemente aus.
+
+// let arr1 = [0,1]
+//
+// let arr2 = arr1
+// for(let i = 1; i<11;i++){
+//     arr2.push(arr1[i]+arr1[i-1])
+// }
+// console.log(arr2)
+// /*
+// [
+//    0,  1,  1,  2,  3,
+//    5,  8, 13, 21, 34,
+//   55, 89
+// ]
+//  */
