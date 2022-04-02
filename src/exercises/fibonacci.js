0f387f35e9e02860f4db456bcec603bed44e05de
Claudia <clodos_m92@hotmail.com>
2022-04-02 18:47:08 +0200

    Fibonacci gelöst

--- src/exercises/fibonacci.js
+++ src/exercises/fibonacci.js
@@ -2,3 +2,17 @@
 
 // Berechne die Fibonacci Folge für n Elemente, beginnend bei 0.
 // Gib alle berechneten Elemente aus.
+function fibonacci (anzahlElemente){
+    let fibonacciArr = [0, 1]
+
+    for (let i = 2; fibonacciArr.length < anzahlElemente; i++) {
+        console.log(i)
+        fibonacciArr.push(fibonacciArr[i-2] + fibonacciArr[i-1])
+    }
+    return fibonacciArr
+}
+
+let result = fibonacci(10)
+console.log(result)
+
+
