f843327be9ce6244bc6778bf30a952c727563f2f
Claudia <clodos_m92@hotmail.com>
2022-04-09 13:45:31 +0200

    Aufgaben closure.js & fn-Objects-callbacks erledigt

--- src/oop-es5.js
+++ src/oop-es5.js
@@ -2,7 +2,7 @@
 // singleton
 let validator = {
     valid: true,
-    validate: function(input){
+    validate: function (input) {
         this.valid = input.indexOf('test') !== -1
         return this.valid
     }
@@ -12,38 +12,39 @@ validator
 
 
 // Konstruktor Funktion
-function Auto(brand){
+function Auto(brand) {
     this.distance = 0
     this.brand = brand
 
     // erstelle für jedes auto ein neues funktionsobjekt
     // => memory leak
-    this.drive = function(distance){
-        console.log('drive instance')
-       this.distance += distance
-    }
+    //   this.drive = function(distance){
+    //     console.log('drive instance', distance)
+    //  this.distance += distance
+    // }
 }
 
 // Besser: erstelle die drive Funktion einmal im Prototyp Objekt,
 // alle Instanzen von Auto verwenden dann diese Funktion
-// Auto.prototype.drive = function(distance){
-//     console.log('drive prototype')
-//     this.distance += distance;
-// }
+Auto.prototype.drive = function (distance) {
+    console.log('drive prototype', distance)
+    this.distance += distance;
+}
 
 let a1 = new Auto('Ford')
 let a2 = new Auto('Tesla')
 a1.drive(10)
 a2.drive(340)
 
-
-// Auto.prototype.drive = function(){
-//     console.log('drive prototype overwrite')
-// }
+//Prototyp funktion überschreiben gilt für alle kinder
+ Auto.prototype.drive = function(){
+     console.log('drive prototype overwrite')
+ }
 
 let a3 = new Auto('Tesla')
 a3.drive(10)
 a1.drive(10)
+
 
 // Die Eigenschaft __proto__ zeigt auf das Prototyp-Objekt.
 // Falls es keine prototype Definition gibt (Ctor.prototype.xy = ...)
